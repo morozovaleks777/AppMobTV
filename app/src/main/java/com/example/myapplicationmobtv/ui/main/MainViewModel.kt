9f1c219a6574bccc74d122659f5a2a61785f849a
oleksandr.o.morozov <morozovaleks@ukr.net>
2021-09-07 14:56:08 +0300

    some refactor

--- app/src/main/java/com/example/myapplicationmobtv/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/myapplicationmobtv/ui/main/MainViewModel.kt
@@ -1,40 +1,37 @@
 package com.example.myapplicationmobtv.ui.main
 
-import android.content.Context
 import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.myapplicationmobtv.DataGenerator
 import com.example.myapplicationmobtv.ParentItem
 import com.example.myapplicationmobtv.SomeData
-import java.io.IOException
-import java.nio.charset.Charset
-import java.util.ArrayList
+import java.util.*
 
 class MainViewModel : ViewModel() {
 
-    fun ParentItemList(): List<ParentItem> {
+    fun parentItemList(): List<ParentItem> {
         val itemList: MutableList<ParentItem> = ArrayList()
         val item = ParentItem(
             "Movies",
-            ChildItemList()[0]
+            childItemList()[0]
         )
         itemList.add(item)
         val item1 = ParentItem(
             "Series",
-            ChildItemList()[1]
+            childItemList()[1]
         )
         itemList.add(item1)
 
         return itemList
     }
-    private fun ChildItemList(): List<List<SomeData>> {
+    private fun childItemList(): List<List<SomeData>> {
         val ChildItemList: List<List<SomeData>> = DataGenerator.getData()
         Log.d("Tag","ChildItemList"+ChildItemList[0])
         Log.d("Tag","ChildItemList"+ChildItemList[1])
         Log.d("Tag","ChildItemList size"+ChildItemList.size)
         return ChildItemList
     }
+
+
 
 }
